ea75fc1615adb5771b6623abb04ccf5203930c7e
Vladimir Saenko <vova.saenko1@gmail.com>
2020-11-07 13:29:22 +0200

    add all

---
+++ Old site-Remake/buttSlider.js
@@ -0,0 +1,9 @@
+
+// "use strict";
+
+// let tittle = document.getElementById('tittle');
+// let img = document.getElementById('image');
+
+// function lifestyle() {
+//    tittle.textContent = "sfs";
+// }
